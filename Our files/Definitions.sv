bea16648cf13e79685586c97962ac55594579d89
Haocheng Li <49082800+Teggoon@users.noreply.github.com>
2020-08-18 21:06:57 -0700

    Modified Definitions to fit our OP codes

--- Our files/Definitions.sv
+++ Our files/Definitions.sv
@@ -6,18 +6,29 @@
 package definitions;
     
 // Instruction map
-    const logic [2:0]kADD  = 3'b000;
-    const logic [2:0]kLSH  = 3'b001;
-    const logic [2:0]kRSH  = 3'b010;
-    const logic [2:0]kXOR  = 3'b011;
-    const logic [2:0]kAND  = 3'b100;
-	const logic [2:0]kSUB  = 3'b101;
-	const logic [2:0]kCLR  = 3'b110;
+	const logic [3:0]kRC_ADD  	= 4'b0000;
+	const logic [3:0]kRC_SUB  	= 4'b0001;
+	const logic [3:0]kRC_LSL  	= 4'b0010;
+	const logic [3:0]kRC_LSR  	= 4'b0011;
+	const logic [3:0]kRC_TRANSFER  	= 4'b0100;
+	const logic [3:0]kRC_CUSTOM  	= 4'b0101;
+	const logic [3:0]kREG_COPY  	= 4'b0110;
+	const logic [3:0]kADD	  	= 4'b0111;
+	const logic [3:0]kSUB	  	= 4'b1000;
+	const logic [3:0]kXOR	  	= 4'b1001;
+	const logic [3:0]kAND	  	= 4'b1010;
+	const logic [3:0]kLSL	  	= 4'b1011;
+	const logic [3:0]kLSR	  	= 4'b1100;
+	const logic [3:0]kMEM_OP  	= 4'b1101;
+	const logic [3:0]kCMP	  	= 4'b1110;
+	const logic [3:0]kBRANCH  	= 4'b1111;
 // enum names will appear in timing diagram
-    typedef enum logic[2:0] {
-        ADD, LSH, RSH, XOR,
-        AND, SUB, CLR } op_mne;
-// note: kADD is of type logic[2:0] (3-bit binary)
+	typedef enum logic[3:0] {
+        RC_ADD, RC_SUB, RC_LSL, RC_LSR, 
+	RC_TRANSFER, RC_CUSTOM, REG_COPY, ADD, 
+	SUB, XOR, AND, LSL, 
+	LSR, MEM_OP, CMP, BRANCH } op_mne;
+	// note: kADD is of type logic[3:0] (4-bit binary)
 //   ADD is of type enum -- equiv., but watch casting
 //   see ALU.sv for how to handle this   
 endpackage // definitions
